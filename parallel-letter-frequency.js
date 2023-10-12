1a1eb29618960aa087370c3ed79071ca93682c6e
Tatiana Kh <ts.khol@gmail.com>
2023-10-12 23:54:54 +0300

    add parallel processing using Promises

--- parallel-letter-frequency.js
+++ parallel-letter-frequency.js
@@ -1,9 +1,40 @@
-export const countFrequency = texts => {
-  const splitTexts = texts.map(text => text.split(''))
+export function countFrequency(texts) {
+  const splitTexts = texts.map(text => [...text])
 
-  const frequency = splitText.reduce((acc, char) => {
-    acc[char] = (acc[char] || 0) + 1
+  const promises = splitTexts.map(splitText => {
+    return new Promise((resolve, reject) => {
+      try {
+        const frequency = splitText.reduce((acc, char) => {
+          acc[char] = (acc[char] || 0) + 1
+          return acc
+        }, {})
+        resolve(frequency)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  })
 
-    return acc
-  }, {})
+  return Promise.all(promises)
+    .then(results => {
+      const totalFrequency = results.flatMap(Object.entries)
+        .reduce((acc, [char, count]) => {
+          acc[char] = (acc[char] || 0) + count
+          return acc
+        }, {})
+      return totalFrequency
+    })
+    .catch(error => {
+      console.error('Error in countFrequency:', error)
+      throw error
+    })
 }
+
+const texts = ['Hello, World!', 'This is a test text']
+countFrequency(texts)
+  .then(result => {
+    console.log(result)
+  })
+  .catch(error => {
+    console.error('Error in countFrequency:', error)
+  })
